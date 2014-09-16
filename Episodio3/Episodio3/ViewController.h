7e8c4360a24c4035f987029ffa8d0ba889a5e2ba
Santiago Gonzalez Causse <sagoca@gmail.com>
2014-09-16 18:09:04 +0200

    Añadido Episodio3

---
+++ Episodio3/Episodio3/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Episodio3
+//
+//  Created by Santiago Gonzalez Causse on 16/09/14.
+//  Copyright (c) 2014 Santiago Gonzalez Causse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Episodio3/Episodio3/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Episodio3
+//
+//  Created by Santiago Gonzalez Causse on 16/09/14.
+//  Copyright (c) 2014 Santiago Gonzalez Causse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
