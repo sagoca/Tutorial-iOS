9814ce4c3bf3355a8f39c22d58907f47025e4fbc
Santiago Gonzalez Causse <sagoca@gmail.com>
2014-09-16 23:41:52 +0200

    Creado Episodio4

---
+++ Episodio4/Episodio4Tests/Episodio4Tests.m
@@ -0,0 +1,34 @@
+//
+//  Episodio4Tests.m
+//  Episodio4Tests
+//
+//  Created by Santiago Gonzalez Causse on 16/09/14.
+//  Copyright (c) 2014 Santiago Gonzalez Causse. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Episodio4Tests : XCTestCase
+
+@end
+
+@implementation Episodio4Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
