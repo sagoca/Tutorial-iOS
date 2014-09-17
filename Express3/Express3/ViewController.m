2b88b1cdfbda4185b2593406afa9c8c38bde9f52
Santiago Gonzalez Causse <sagoca@gmail.com>
2014-09-17 16:37:17 +0200

    Completado Express3

--- Express3/Express3/ViewController.m
+++ Express3/Express3/ViewController.m
@@ -18,6 +18,25 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    NSArray *array = [NSArray arrayWithObjects:@"primero",@"segundo",@"tercero", nil];
+    NSLog(@"%@", array);
+    
+    NSMutableArray *arraym = [NSMutableArray arrayWithObjects:@"pera",@"manzana", nil];
+    NSLog(@"%@",arraym);
+    [arraym addObject:@"melon"];
+    NSLog(@"%@",arraym);
+    
+    NSDictionary *diccionario = [NSDictionary dictionaryWithObjects:arraym forKeys:array];
+    NSLog(@"%@",diccionario);
+    NSLog(@"%@", [diccionario objectForKey:@"segundo"]);
+    
+    NSMutableDictionary *diccionarioM = [[NSMutableDictionary alloc] init];
+    [diccionarioM setValue:@"Mario" forKey:@"rey del mundo"];
+    NSLog(@"%@",diccionarioM);
+    [diccionarioM setValue:array forKey:@"prueba array"];
+    NSLog(@"%@",diccionarioM);
+
 }
 
 - (void)didReceiveMemoryWarning
