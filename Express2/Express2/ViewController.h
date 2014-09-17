9d67abf190516fd88e554163b88fe10b6c3ee433
Santiago Gonzalez Causse <sagoca@gmail.com>
2014-09-17 11:41:05 +0200

    Completado Express2

--- Express2/Express2/ViewController.h
+++ Express2/Express2/ViewController.h
@@ -10,4 +10,16 @@
 
 @interface ViewController : UIViewController
 
+//Método que no devulve nada ni recibe nada por parámetros
+- (void) metodo1;
+//Método que devuelve un entero
+- (int) metodo2;
+//Método que devuelve un objeto string
+- (NSString *) metodo3;
+//Método que no devuelve nada y recibe un string por parámetros
+- (void) metodo4: (NSString *) prop1;
+//Método que devuelve un NSNumber a partir de un int
+-(NSNumber *) metodo5:(int) entero1;
+//Método con varios parámetros de entrada
+-(void) crearRectangulo: (int) lado conAltura: (int) altura;
 @end
--- Express2/Express2/ViewController.m
+++ Express2/Express2/ViewController.m
@@ -18,6 +18,10 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    //Llamada a método 4
+    [self   metodo4:@"Hola"];
+    //Llamada a método crearRectangulo
+    [self crearRectangulo:3 conAltura:5];
 }
 
 - (void)didReceiveMemoryWarning
@@ -25,5 +29,26 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (void) metodo1{
+    //código para el metodo1
+}
 
+- (int) metodo2{
+    //código para metodo2
+    return 4;
+}
+- (NSString *) metodo3{
+    NSString *devolver = [[NSString alloc] init];
+    return devolver;
+}
+- (void) metodo4: (NSString *) prop1{
+    //código
+}
+-(NSNumber *) metodo5:(int) entero1{
+    NSNumber *numero = [NSNumber numberWithInt:entero1];
+    return numero;
+}
+-(void) crearRectangulo: (int) lado conAltura: (int) altura{
+    
+}
 @end
