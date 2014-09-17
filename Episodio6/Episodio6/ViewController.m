13c78804538e389a385492e89d76ca9b422f02cc
Santiago Gonzalez Causse <sagoca@gmail.com>
2014-09-17 16:43:13 +0200

    Añadido Episodio6

---
+++ Episodio6/Episodio6/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Episodio6
+//
+//  Created by Santiago Gonzalez Causse on 17/09/14.
+//  Copyright (c) 2014 Santiago Gonzalez Causse. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
