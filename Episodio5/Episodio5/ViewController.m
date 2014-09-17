2f5cc75c04420e853c727c575a9ee92ceab95a59
Santiago Gonzalez Causse <sagoca@gmail.com>
2014-09-17 12:44:53 +0200

    Completado Episodio5

--- Episodio5/Episodio5/ViewController.m
+++ Episodio5/Episodio5/ViewController.m
@@ -26,4 +26,44 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (NSInteger) numberOfSectionsInTableView:(UITableView *) tableView{
+    return 1;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return 32;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    static NSString *CellIdentifier = @"Cell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if(cell == nil){
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:CellIdentifier];
+    }
+    
+    //configure the cell...
+    cell.textLabel.text = @"HOLA TABLA";
+    cell.detailTextLabel.text = @"Detalle";
+    cell.imageView.image = [UIImage imageNamed:@"Imagen.png"];
+    cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    return cell;
+}
+
+//Controla qué haces cuando seleccionas una fila
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    //Navigation logic may go here. Create and push another view controler.
+    /*
+     DetailViewController *detailViewControler = [[DetailViewController alloc] initWithNibName:@"Nib name" bundle: nil];
+     //...
+     //Pass the selected object to the new view controller.
+     [self.navigationController pushViewController:detailViewController animated:YES];
+     [detailViewController release];
+     */
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    return 44;
+}
+
 @end
