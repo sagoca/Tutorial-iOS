a2861037d4f163a03b738c3c58cdfb2c8b689911
Santiago Gonzalez Causse <sagoca@gmail.com>
2014-09-18 16:24:54 +0200

    Completado Express4

--- Express4/Express4/ViewController.m
+++ Express4/Express4/ViewController.m
@@ -18,6 +18,40 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    int i = 0;
+    if (i<10) {
+        NSLog(@"i es menor que 10");
+    } else {
+        NSLog(@"i es mayor que 10");
+    }
+    
+    for (i=0; i<10; i++) {
+        NSLog(@"%i",i);
+    }
+    
+    do {
+        NSLog(@"%i",i);
+        i++;
+    } while (i<11);
+    
+    while (i<15) {
+        NSLog(@"%i",i);
+        i++;
+    }
+    switch (i) {
+        case 1:
+            NSLog(@"Tiene valor 1");
+            break;
+        case 2:
+            NSLog(@"Tiene valor 2");
+            break;
+        case 15:
+            NSLog(@"Tiene valor 15");
+            break;
+        default:
+            break;
+    }
 }
 
 - (void)didReceiveMemoryWarning
