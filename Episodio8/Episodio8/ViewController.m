1dcebc73ace100f2a5f0ee6c8bb97bb5c0571e50
Santiago Gonzalez Causse <sagoca@gmail.com>
2014-09-19 16:25:52 +0200

    Completado Episodio8

--- Episodio8/Episodio8/ViewController.m
+++ Episodio8/Episodio8/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "VistaNuevaViewController.h"
 
 @interface ViewController ()
 
@@ -24,6 +25,14 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender{
+    if ([[segue identifier] isEqualToString:@"segueVistaRoja"]) {
+        NSLog(@"Está pasando");
+        VistaNuevaViewController *vc = [segue destinationViewController];
+        [vc.view setBackgroundColor:[UIColor blueColor]];
+    }
 }
 
 @end
---
+++ Episodio8/Episodio8/VistaNuevaViewController.m
@@ -0,0 +1,49 @@
+//
+//  VistaNuevaViewController.m
+//  Episodio8
+//
+//  Created by Santiago Gonzalez Causse on 19/09/14.
+//  Copyright (c) 2014 Santiago Gonzalez Causse. All rights reserved.
+//
+
+#import "VistaNuevaViewController.h"
+
+@interface VistaNuevaViewController ()
+
+@end
+
+@implementation VistaNuevaViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
