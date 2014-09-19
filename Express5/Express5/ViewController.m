fa402b126c97bcc318769c63eb3de9e235a38a92
Santiago Gonzalez Causse <sagoca@gmail.com>
2014-09-19 12:33:46 +0200

    Completado Express5

--- Express5/Express5/ViewController.m
+++ Express5/Express5/ViewController.m
@@ -14,14 +14,26 @@
 
 @implementation ViewController
 
+@synthesize vistaRoja;
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    //Para que funcione hay que desactivar el auto layout de la vista en la pestaña file inspector
+    
+    //[self.vistaRoja setFrame:CGRectMake(self.vistaRoja.frame.origin.x, self.vistaRoja.frame.origin.y, 100, 300)]; //esto es para cambiar el tamaño pero mantener la posición
+    [UIView animateWithDuration:1.0 animations:^{
+        [self.vistaRoja setFrame:CGRectMake(200, 80, self.vistaRoja.frame.size.width, self.vistaRoja.frame.size.height)];
+    }];
+    
+    
 }
 
 - (void)didReceiveMemoryWarning
 {
+    
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
