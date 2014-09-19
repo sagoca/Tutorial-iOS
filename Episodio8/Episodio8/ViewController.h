a49e66b9d3de0355c4ccd0105c69b114a8d2d0cc
Santiago Gonzalez Causse <sagoca@gmail.com>
2014-09-19 15:55:21 +0200

    Creado Episodio8

---
+++ Episodio8/Episodio8/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Episodio8
+//
+//  Created by Santiago Gonzalez Causse on 19/09/14.
+//  Copyright (c) 2014 Santiago Gonzalez Causse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Episodio8/Episodio8Tests/Episodio8Tests.m
@@ -0,0 +1,34 @@
+//
+//  Episodio8Tests.m
+//  Episodio8Tests
+//
+//  Created by Santiago Gonzalez Causse on 19/09/14.
+//  Copyright (c) 2014 Santiago Gonzalez Causse. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Episodio8Tests : XCTestCase
+
+@end
+
+@implementation Episodio8Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
