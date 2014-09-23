be1fd2900e3efe997b3b8357656db7f04621c6bd
Santiago Gonzalez Causse <sagoca@gmail.com>
2014-09-23 17:36:22 +0200

    Copiado Episodio9 en Episodio10

--- Episodio10/Episodio10/ViewController.h
+++ Episodio10/Episodio10/ViewController.h
@@ -7,8 +7,11 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+- (IBAction)pulsarBoton:(id)sender;
 
 
 @end
--- Episodio10/Episodio10/ViewController.m
+++ Episodio10/Episodio10/ViewController.m
@@ -10,13 +10,36 @@
 
 @interface ViewController ()
 
+
 @end
 
+
 @implementation ViewController
+
+@synthesize mapView;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    MKCoordinateRegion region; //Región en la que se va a centrar el mapa
+    MKCoordinateSpan span; //Altitud
+    span.latitudeDelta=0.2;
+    span.longitudeDelta=0.2;
+    CLLocationCoordinate2D location;
+    location.latitude=37.3859505;
+    location.longitude=-6.0764275;
+    region.span = span;
+    region.center = location;
+    [mapView setRegion:region animated:YES];
+    [mapView regionThatFits:region];
+    
+    CLLocationCoordinate2D coordPunto = CLLocationCoordinate2DMake(region.center.latitude, region.center.longitude);
+    MKPointAnnotation *anotacion = [[MKPointAnnotation alloc] init];
+    [anotacion setCoordinate:coordPunto];
+    [anotacion setTitle:@"Mi casa"];
+    [anotacion setSubtitle:@"c/ Pio XII"];
+    [self.mapView addAnnotation:anotacion];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,4 +47,20 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)pulsarBoton:(id)sender {
+    UIBarButtonItem *boton = (UIBarButtonItem *) sender;
+    switch (boton.tag) {
+        case 1:
+            [mapView setMapType:MKMapTypeSatellite];
+            break;
+        case 2:
+            [mapView setMapType:MKMapTypeStandard];
+            break;
+        case 3:
+            [mapView setMapType:MKMapTypeHybrid];
+            break;
+        default:
+            break;
+    }
+}
 @end
