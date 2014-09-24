ebca056565a4f489bda104b0ba1635358d53dc80
Santiago Gonzalez Causse <sagoca@gmail.com>
2014-09-24 10:39:55 +0200

    Creado Express7

---
+++ Express7/Express7/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Express7
+//
+//  Created by Santiago Gonzalez Causse on 24/09/14.
+//  Copyright (c) 2014 Santiago Gonzalez Causse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Express7/Express7/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Express7
+//
+//  Created by Santiago Gonzalez Causse on 24/09/14.
+//  Copyright (c) 2014 Santiago Gonzalez Causse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
