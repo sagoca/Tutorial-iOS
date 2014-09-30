7a4880cfd2239ad3482c4da906f591f5faf1ec67
Santiago Gonzalez Causse <sagoca@gmail.com>
2014-09-30 11:22:15 +0200

    Creado Ejercicio1

---
+++ Ejercicio1/Ejercicio1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Ejercicio1
+//
+//  Created by Santiago Gonzalez Causse on 30/09/14.
+//  Copyright (c) 2014 Santiago Gonzalez Causse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
