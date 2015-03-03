b5d4100f45be4ee25ee65e940a0668b33d25c709
Santiago Gonzalez Causse <sagoca@gmail.com>
2015-03-03 16:38:37 +0100

    Hecha la carga de imagen a partir de una url

--- Episodio12/Episodio12/ViewController.m
+++ Episodio12/Episodio12/ViewController.m
@@ -13,6 +13,7 @@
 @end
 
 @implementation ViewController
+@synthesize imagen;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -24,4 +25,11 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)pulsarBotonCargarImagen:(id)sender {
+    NSURL *url = [NSURL URLWithString:@"http://www.newstatesman.com/sites/default/files/images/2014%2B36_Friends_Cast_Poker%281%29.jpg"];
+    NSData *data = [NSData dataWithContentsOfURL:url];
+    
+    [self.imagen setImage:[UIImage imageWithData:data]];
+    
+}
 @end
