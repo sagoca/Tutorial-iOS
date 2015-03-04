67dc79ac84166c6b3c6fe1751f707c0428fa8a08
Santiago Gonzalez Causse <sagoca@gmail.com>
2015-03-04 10:08:35 +0100

    Creado Episodio13 y añadida base de datos.

---
+++ Episodio13/Episodio13/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Episodio13
+//
+//  Created by Santiago Gonzalez Causse on 4/3/15.
+//  Copyright (c) 2015 Santiago Gonzalez Causse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Episodio13/Episodio13/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Episodio13
+//
+//  Created by Santiago Gonzalez Causse on 4/3/15.
+//  Copyright (c) 2015 Santiago Gonzalez Causse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
