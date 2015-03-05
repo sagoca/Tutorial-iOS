5901a521fa6dbdae805ef530af2f13c4bd365a03
Santiago Gonzalez Causse <sagoca@gmail.com>
2015-03-05 15:40:02 +0100

    Creado episodio 15

---
+++ Episodio15/Episodio15/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Episodio15
+//
+//  Created by Santiago Gonzalez Causse on 5/3/15.
+//  Copyright (c) 2015 Santiago Gonzalez Causse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
